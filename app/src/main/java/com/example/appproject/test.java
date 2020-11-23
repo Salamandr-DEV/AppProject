f8ac7a3ec2f2e8a519118ba305e34d672d175789
Kotenka <37335759+Kotenka@users.noreply.github.com>
2020-11-23 12:04:35 +0300

    help
    перевод работает в зависимости от выбранного языка на телефоне. при нажатии на кнопку помощь приложение падает. хехе

--- app/src/main/java/com/example/appproject/test.java
+++ app/src/main/java/com/example/appproject/test.java
@@ -1,41 +1,21 @@
 package com.example.appproject;
 
-import androidx.annotation.RequiresApi;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.os.Build;
-import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.app.Activity;
-import android.content.res.Configuration;
-import android.os.Bundle;
-
-import java.util.Locale;
-
 public class test extends AppCompatActivity{
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-//            String languageToLoad  = "en"; // your language
-//            Locale locale = new Locale(languageToLoad);
-//            Locale.setDefault(locale);
-//            Configuration config = new Configuration();
-//            config.locale = locale;
-//            getBaseContext().getResources().updateConfiguration(config,
-//                    getBaseContext().getResources().getDisplayMetrics());
-//            this.setContentView(R.layout.activity_test);
-
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -48,7 +28,7 @@ public class test extends AppCompatActivity{
         CardView randomcard = (CardView) findViewById(R.id.random_card);
         CardView helpcard = (CardView) findViewById(R.id.help_card);
 
-        Button langbutton = (Button)  findViewById(R.id.lang_button);
+
 
         yesnocard.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,9 +56,6 @@ public class test extends AppCompatActivity{
         });
 
 
-
-
-
     }
     public void openYesNo2() {
         Intent i;
@@ -99,6 +76,9 @@ public class test extends AppCompatActivity{
     }
 
     public void openFullscreenActivity() {
+        Intent i;
+        i = new Intent(this, help.class);
+        startActivity(i);
 
     }
 
